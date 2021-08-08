d38dd907dce67f9bab69498788f753baf2091f33
Tatr1 <ttttt_68@mail.ru>
2021-08-08 15:52:40 +0300

    edited report

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "6762718457988246";
+        String number = "6011052744571881";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
